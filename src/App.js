262fb6a419b75d129a4e85da854209be930a210d
Benji Mirschel <cliff.mirschel@gmail.com>
2019-08-03 10:27:52 -0400

    Added basic stylings using Tachyons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from "react";
-import Amplify from "aws-amplify";
+import Amplify, { API, graphqlOperation } from "aws-amplify";
 import aws_exports from "./aws-exports";
 import { withAuthenticator } from "aws-amplify-react"; // or 'aws-amplify-react-native';
+import { listNotes } from "./graphql/queries";
 
 Amplify.configure(aws_exports);
 
 const App = () => {
-  return <div>Hello</div>;
+  return (
+    <div className="flex flex-column items-center justify-center bg-washed-red pa3">
+      <h1 className="code f2">Amplify Notetaker</h1>
+      <form className="mb3">
+        <input type="text" placeholder="Write your note" className="pa2 f4" />
+        <button type="submit" className="pa2 f4">
+          Add
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default withAuthenticator(App, { includeGreetings: true });
