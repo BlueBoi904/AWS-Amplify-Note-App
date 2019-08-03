1aaa9f52e6f266f3b44d8e03456b12a80478421f
Benji Mirschel <cliff.mirschel@gmail.com>
2019-08-03 10:13:28 -0400

    imported and added withAuth from Amplify to our project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Amplify from "aws-amplify";
 import aws_exports from "./aws-exports";
+import { withAuthenticator } from "aws-amplify-react"; // or 'aws-amplify-react-native';
+
 Amplify.configure(aws_exports);
 
 const App = () => {
   return <div>Hello</div>;
 };
 
-export default App;
+export default withAuthenticator(App, { includeGreetings: true });
